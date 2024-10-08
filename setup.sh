9a1d9d5e8c4574a992a510d71838fdd3bbc4105b
EducatedSuddenBucket <educatedsuddenbucket@gmail.com>
2024-10-08 22:47:25 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,31 +1,6 @@
 #!/bin/bash
-
-# Function to detect architecture
-detect_arch() {
-  arch=$(uname -m)
-  case $arch in
-    x86_64)
-      jq_url="https://github.com/stedolan/jq/releases/latest/download/jq-linux64"
-      ;;
-    aarch64)
-      jq_url="https://github.com/stedolan/jq/releases/latest/download/jq-linux64"
-      ;;
-    armv7l)
-      jq_url="https://github.com/stedolan/jq/releases/latest/download/jq-linux32"
-      ;;
-    *)
-      echo "Unsupported architecture: $arch"
-      exit 1
-      ;;
-  esac
-}
-
-# Detect system architecture
-detect_arch
-
-# Download jq based on the system architecture
-curl -Lo jq $jq_url && chmod +x jq
-
+# Download jq
+curl -Lo jq https://github.com/stedolan/jq/releases/latest/download/jq-linux64 && chmod +x jq
 # Fetch the country name using curl and jq
 country_name=$(curl -s https://ipapi.co/json/ | ./jq -r '.country_name')
 
